c8c1ae3d4f66c7c895567c070f603710e4f7657b
sunakan <sunakan03@gmail.com>
2019-09-01 12:15:18 +0900

    [Add] fluent.confの簡単なメモ

---
+++ 03-fluentd-conf/date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ====`date "+%Y%m%d-%Hh%Mm%Ss"` >&1
+sleep 2
+echo [INFO ] `date "+%Y%m%d-%Hh%Mm%Ss"` >&1
+sleep 1
+echo [ERROR] `date "+%Y%m%d-%Hh%Mm%Ss"` >&2
+sleep 1
+echo [INFO ] `date "+%Y%m%d-%Hh%Mm%Ss"` >&1
+sleep 1
+echo [ERROR] `date "+%Y%m%d-%Hh%Mm%Ss"` >&2
+sleep 1
+echo [INFO ] `date "+%Y%m%d-%Hh%Mm%Ss"` >&1
