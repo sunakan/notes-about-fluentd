5dcd808b7ebbbd2bd63de7ac4d13abd20ef3dfa2
sunakan <sunakan03@gmail.com>
2019-10-06 22:36:48 +0900

    [Fix] STDERRだけど文字列にERRORが入ってないechoの内容を変更

--- 06-slack/sample.sh
+++ 06-slack/sample.sh
@@ -12,7 +12,7 @@ echo [ERROR] `date "+%Y%m%d-%Hh%Mm%Ss"` >&2
 sleep 1
 echo [INFO ] `date "+%Y%m%d-%Hh%Mm%Ss"` >&1
 sleep 1
-echo [INFO ] fake fake >&2
+echo stderr, but no error string >&2
 ls aaaaaaaa
 echo [INFO ] Hello
 echo [INFO ] Hello
