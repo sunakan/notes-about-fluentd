adfef808d73f7f83f976c61baeefc77a8ca4ae24
sunakan <sunakan03@gmail.com>
2019-10-06 22:33:13 +0900

    [Fix] fluent & slackのサンプルを更新

--- 06-slack/sample.sh
+++ 06-slack/sample.sh
@@ -11,3 +11,9 @@ sleep 1
 echo [ERROR] `date "+%Y%m%d-%Hh%Mm%Ss"` >&2
 sleep 1
 echo [INFO ] `date "+%Y%m%d-%Hh%Mm%Ss"` >&1
+sleep 1
+echo [INFO ] fake fake >&2
+ls aaaaaaaa
+echo [INFO ] Hello
+echo [INFO ] Hello
+echo [INFO ] Hello
